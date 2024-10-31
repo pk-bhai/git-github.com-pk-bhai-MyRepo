9acad734818da7ecdcd39fb574fc8f6d14b6020d
Krishna <pranaykrishna0103@gmail.com>
2024-10-31 10:44:21 -0500

    First Commit

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+//Line 2 to 6 ---Boiler Plate code
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Hello World" << endl;
+
+    bool  isSafe = true;
+    cout << isSafe << endl;
+    return 0;
+}
