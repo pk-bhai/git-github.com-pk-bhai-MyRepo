cd91e94eb393f73e82566a41f0327e5e3a63ab18
Krishna <pranaykrishna0103@gmail.com>
2024-10-31 11:08:42 -0500

    if-else code

--- demo.cpp
+++ demo.cpp
@@ -4,9 +4,29 @@ using namespace std;
 
 int main()
 {
+    // First code
     cout << " Hello World" << endl;
 
+// Data types
     bool  isSafe = true;
     cout << isSafe << endl;
+
+// If and else
+    int a = 18;
+    if (a<18){
+        cout<< "You are a Minor" << endl;
+    }else
+    cout << "You are a Major. Yay!!" << endl;
+//Odd and Even
+
+    if(a%2 == 0){
+        cout << "Even" <<endl;
+    }
+    else
+    {
+        cout << " Odd" << endl;
+    }
+
+
     return 0;
 }
